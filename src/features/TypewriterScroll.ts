17948946725a9f25221773ca3ae528bc51f322e3
Davis Riedel <mail@davis-riedel.de>
2023-04-12 08:25:00 +0200

    fixes #28

--- src/features/TypewriterScroll.ts
+++ src/features/TypewriterScroll.ts
@@ -3,7 +3,7 @@ import { TypewriterModeSettings } from "@/TypewriterModeSettings";
 
 export default class TypewriterScroll extends FeatureToggle {
   protected setting: keyof TypewriterModeSettings = "isTypewriterScrollEnabled";
-  protected override toggleClass = "ptm";
+  protected override toggleClass = "ptm-typewriter-scroll";
   protected requiresReload = true;
   protected hasCommand = true;
   protected override commandTitle = "Toggle Typewriter Scrolling On/Off";
