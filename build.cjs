0840c300d0c770bd96c3d4095223c758916c1446
Davis Riedel <mail@davis-riedel.de>
2023-12-06 15:11:07 +0100

    Add test vault folder

--- build.cjs
+++ build.cjs
@@ -2,11 +2,12 @@
 
 const { nodeExternalsPlugin } = require("esbuild-node-externals");
 const { sassPlugin } = require("esbuild-sass-plugin");
+const fs = require("fs");
 
 require("esbuild")
   .build({
     entryPoints: ["./src/main.ts", "./src/styles.scss"],
-    outdir: ".",
+    outdir: "./dist",
     bundle: true,
     minify: true,
     platform: "node",
@@ -14,4 +15,8 @@ require("esbuild")
     target: "node14",
     plugins: [nodeExternalsPlugin(), sassPlugin()],
   })
-  .catch(() => process.exit(1));
+  .catch(() => process.exit(1))
+  .finally(() => {
+    fs.copyFileSync("./manifest.json", "./dist/manifest.json");
+  });
+
